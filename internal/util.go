74a8125bc7e4444086de86c88038d14c034225a0
Fabulous Fadz <fadzai.chamba@outlook.com>
2023-12-30 02:24:00 +0200

    Common utility methods

--- internal/util.go
+++ internal/util.go
@@ -1,6 +1,15 @@
 package internal
 
+import "log"
+
 func IsDigit(input byte) bool {
 	res := input - '0'
 	return res <= 9
+}
+
+func Must[T interface{}](result T, err error) T {
+	if err != nil {
+		log.Panicf("Could not complete operation. %v", err)
+	}
+	return result
 }
