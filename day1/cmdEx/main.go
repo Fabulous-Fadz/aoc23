c413ff6d632cef609cfa610c8361e5e774402577
Fabulous Fadz <fadzai.chamba@outlook.com>
2024-01-18 03:43:14 +0200

    Added profile logic depending on the existence of a command line flag.

--- day1/cmdEx/main.go
+++ day1/cmdEx/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"runtime/pprof"
 	"strings"
 
 	"github.com/Fabulous-Fadz/aoc23/day1/trebuchet"
 	"github.com/Fabulous-Fadz/aoc23/internal"
 )
 
-var fileData = internal.Must(os.ReadFile("../input.txt"))
+var (
+	profile  = flag.Bool("profile", false, "Should we run a profiler on this?")
+	fileData = internal.Must(os.ReadFile("../input.txt"))
+)
 
 func main() {
+	flag.Parse()
+	if *profile {
+		cpuProfile := internal.Must(os.Create("cpuprofile"))
+		memProfile := internal.Must(os.Create("memprofile"))
+		if err := pprof.StartCPUProfile(cpuProfile); err == nil {
+			defer pprof.StopCPUProfile()
+		}
+		defer pprof.WriteHeapProfile(memProfile)
+	}
+
 	items := [][]byte{}
 
 	foo := strings.Split(string(fileData), "\n")
